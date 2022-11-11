62b347cd07f3bcc37afc3d84aefb42a92ce681b2
Wheels <85713658+Wheels6666@users.noreply.github.com>
2022-11-11 18:29:09 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript: var host = httpReq = new XMLHttpRequest(); httpReq.open("GET", "https://raw.githubusercontent.com/Wheels6666/edpuzzle-cheats/main/updatedScript.js", true); httpReq.addEventListener("load", function(){eval(this.responseText);}); httpReq.send();
+javascript: httpReq = new XMLHttpRequest(); httpReq.open("GET", "https://raw.githubusercontent.com/Wheels6666/edpuzzle-cheats/main/updatedScript.js", true); httpReq.addEventListener("load", function(){eval(this.responseText);}); httpReq.send();
